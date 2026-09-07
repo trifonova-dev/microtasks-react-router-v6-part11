c11cc6911f42fb748510a9ec2ab8dea97323fa82
Natalia Trifonova <trifonova.dev@gmail.com>
2026-09-07 16:15:39 +0700

    train

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,26 +5,30 @@ import {Adidas} from "../components/pages/Adidas";
 import {Puma} from "../components/pages/Puma.tsx";
 import {Abibas} from "../components/pages/Abibas.tsx";
 import {Prices} from "../components/pages/Prices.tsx";
+import {Model} from "../components/pages/Model";
 
 const PATH = {
-  ADIDAS: '/adidas',
-  PUMA: '/puma',
-  ABIBAS: '/abibas',
-  PRICES: '/prices',
+    ADIDAS: '/adidas',
+    PUMA: '/puma',
+    ABIBAS: '/abibas',
+    PRICES: '/prices',
+    MODEL: '/:model/:id'
 
 } as const
 
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    errorElement: <Error404/>,
-    children: [
-      { path: PATH.ADIDAS, element: (<Adidas/>) },
-      { path: PATH.PUMA, element: (<Puma/>) },
-      { path: PATH.ABIBAS, element: (<Abibas/>) },
-      { path: PATH.PRICES, element: (<Prices/>) },
-    ],
-  },
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <Error404/>,
+        children: [
+            {path: PATH.ADIDAS, element: (<Adidas/>)},
+            {path: PATH.PUMA, element: (<Puma/>)},
+            {path: PATH.ABIBAS, element: (<Abibas/>)},
+            {path: PATH.PRICES, element: (<Prices/>)},
+            {path: PATH.MODEL, element: (<Model/>)},
+
+        ],
+    },
 ]);
